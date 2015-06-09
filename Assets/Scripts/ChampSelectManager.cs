368ee5a39f913c34795d79b764044624a5ed31e9
Sunjeev Devulapalli <svd12@duke.edu>
2015-06-08 22:24:13 -0400

    added basic layout to champ select

---
+++ Assets/Scripts/ChampSelectManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class will load in champions from some set of datafiles. From these datafiles gets pulled a list of the names of the champions
+//Then map to an image. This class will then assign the positions for the champions on screen. 
+
+//This class will start the timer script and hold the action for what happens when it hits zero. If you do not select a champion, this class assigns 
+//the player one. 
+
+public class ChampSelectManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//all data needs to be loaded here from an xml file (presumably using another class to parse xml)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * This class is in charge of counting down the Time Limit Text. So every second it will decrease the timelimit until zero. Once it hits zero
+ * it needs to call its method.
+ * 
+ * This class has a public method that's passed into it which is what is called when the timer is zero. 
+ * 
+ * */
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
