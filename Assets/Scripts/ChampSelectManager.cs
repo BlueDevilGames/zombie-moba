8362a6e9c6ccfab2f10ed874bdcde36d98ccd13c
Qian Wang <qwang2012@gmail.com>
2015-06-13 00:27:43 -0700

    Added JSON data loading
    Added test JSON file which loads champion names and AD to screen

--- Assets/Scripts/ChampSelectManager.cs
+++ Assets/Scripts/ChampSelectManager.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
+using System.IO;
+using SimpleJSON;
 
 //This class will load in champions from some set of datafiles. From these datafiles gets pulled a list of the names of the champions
 //Then map to an image. This class will then assign the positions for the champions on screen. 
@@ -7,15 +11,41 @@ using System.Collections;
 //This class will start the timer script and hold the action for what happens when it hits zero. If you do not select a champion, this class assigns 
 //the player one. 
 
-public class ChampSelectManager : MonoBehaviour {
+public class ChampSelectManager : MonoBehaviour
+{
+	public Text[] nameLabels;
 
 	// Use this for initialization
-	void Start () {
-		//all data needs to be loaded here from an xml file (presumably using another class to parse xml)
+	void Start ()
+	{
+		//TODO: all data needs to be loaded here from an xml file (presumably using another class to parse xml)
+
+		// Load champion data from JSON file
+		string data = this.ReadFileToString ("Data\\champion_simple.json");
+
+		JSONNode jsonData = JSON.Parse (data);
+		for (int i=0; i<10; i++) {
+			nameLabels [i].text = jsonData [i] ["name"] + "\nAD: " + jsonData [i] ["attack"];
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
+	}
+
+	/// <summary>
+	/// Reads the entire file to a string.
+	/// </summary>
+	/// <returns>string of the file contents</returns>
+	/// <param name="path">Path to the file to be read</param>
+	string ReadFileToString (string path)
+	{
+		string str;
+		using (StreamReader streamReader = new StreamReader(path)) {
+			str = streamReader.ReadToEnd ();
+		}
+		return str;
 	}
 }
