6576cdf90080040b263c9edfe6c03693e17ac611
Qian Wang <qwang2012@gmail.com>
2015-06-17 19:00:53 -0700

    Added tutorial script from palladiumgames

---
+++ Assets/Scripts/cubeMoveAuthoritative.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeMoveAuthoritative : MonoBehaviour
+{
+	
+	public NetworkPlayer theOwner;
+	float lastClientHInput = 0f;
+	float lastClientVInput = 0f;
+	float serverCurrentHInput = 0f;
+	float serverCurrentVInput = 0f;
+	
+	void Awake ()
+	{
+		if (Network.isClient)
+			enabled = false;
+	}
+	
+	[RPC]
+	void SetPlayer (NetworkPlayer player)
+	{
+		theOwner = player;
+		if (player == Network.player)
+			enabled = true;
+	}
+	
+	void Update ()
+	{
+		if (theOwner != null && Network.player == theOwner) {
+			float HInput = Input.GetAxis ("Horizontal");
+			float VInput = Input.GetAxis ("Vertical");
+			if (lastClientHInput != HInput || lastClientVInput != VInput) {
+				lastClientHInput = HInput;
+				lastClientVInput = VInput;
+			}
+			if (Network.isServer) {
+				SendMovementInput (HInput, VInput);
+			} else if (Network.isClient) {
+				GetComponent<NetworkView>().RPC ("SendMovementInput", RPCMode.Server, HInput, VInput);
+			}
+		}
+		if (Network.isServer) {
+			Vector3 moveDirection = new Vector3 (serverCurrentHInput, 0, serverCurrentVInput);
+			float speed = 5;
+			transform.Translate (speed * moveDirection * Time.deltaTime);
+		}
+	}
+	
+	[RPC]
+	void SendMovementInput (float HInput, float VInput)
+	{  
+		serverCurrentHInput = HInput;
+		serverCurrentVInput = VInput;
+	}
+	
+	void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info)
+	{
+		if (stream.isWriting) {
+			Vector3 pos = transform.position;          
+			stream.Serialize (ref pos);
+		} else {
+			Vector3 posReceive = Vector3.zero;
+			stream.Serialize (ref posReceive);
+			transform.position = posReceive;
+		}
+	}
+}
