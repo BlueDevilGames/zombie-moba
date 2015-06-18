7213537d6462c9499a26a5d50f79e047ead04783
Qian Wang <qwang2012@gmail.com>
2015-06-17 19:41:41 -0700

    Updated to use tutorial at
    http://www.paladinstudios.com/2013/07/10/how-to-create-an-online-multiplayer-game-with-unity/

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+	public GameObject Player;
+
+	private const string typeName = "Zombies";
+	private const string gameName = "TestMOBAGame";
+	
+	private void SpawnPlayer ()
+	{
+		Network.Instantiate (Player, new Vector3 (0f, 5f, 0f), Quaternion.identity, 0);
+	}
+
+	private void StartServer ()
+	{
+		Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (typeName, gameName);
+	}
+
+	void OnServerInitialized ()
+	{
+		SpawnPlayer ();
+		Debug.Log ("Server Initializied");
+	}
+	void OnGUI ()
+	{
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
+				StartServer ();
+			
+			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList ();
+			
+			if (hostList != null) {
+				for (int i = 0; i < hostList.Length; i++) {
+					if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList [i].gameName))
+						JoinServer (hostList [i]);
+				}
+			}
+		}
+	}
+
+	private HostData[] hostList;
+	
+	private void RefreshHostList ()
+	{
+		MasterServer.RequestHostList (typeName);
+	}
+	
+	void OnMasterServerEvent (MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList ();
+	}
+
+	private void JoinServer (HostData hostData)
+	{
+		Network.Connect (hostData);
+	}
+	
+	void OnConnectedToServer ()
+	{
+		Debug.Log ("Server Joined");
+		SpawnPlayer ();
+	}
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 10f;
+	
+	void Update ()
+	{
+		if (GetComponent<NetworkView>().isMine) {
+			InputMovement ();
+		}
+	}
+	
+	void InputMovement ()
+	{
+		if (Input.GetKey (KeyCode.W))
+			GetComponent<Rigidbody> ().MovePosition (GetComponent<Rigidbody> ().position + Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey (KeyCode.S))
+			GetComponent<Rigidbody> ().MovePosition (GetComponent<Rigidbody> ().position - Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey (KeyCode.D))
+			GetComponent<Rigidbody> ().MovePosition (GetComponent<Rigidbody> ().position + Vector3.right * speed * Time.deltaTime);
+		
+		if (Input.GetKey (KeyCode.A))
+			GetComponent<Rigidbody> ().MovePosition (GetComponent<Rigidbody> ().position - Vector3.right * speed * Time.deltaTime);
+	}
+}
