2cfd6e8927a6f6ee402adee05f8d9b3ec8864059
Sunjeev Devulapalli <svd12@duke.edu>
2015-06-19 16:11:58 -0400

    Title Screen and Login Combined
    You can input a username and click the play button. Error checking so
    far consists solely of checking if a username is added but once we get
    networking done, we need to check for duplicate usernames somehow.

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleScreenManager : MonoBehaviour {
+
+	public GameObject errorText;
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onPlay(InputField userNameInput) {
+		if (!userNameInput.textComponent.text.Equals ("")) {
+			Debug.Log (userNameInput.textComponent.text);
+//			Application.LoadLevel("Champ Select Screen");
+		} else {
+			GameObject obj = (GameObject) Instantiate(errorText, new Vector3(0, 0, 0), Quaternion.identity);
+			obj.transform.SetParent(canvas.transform, false);
+		}
+	}
+}
