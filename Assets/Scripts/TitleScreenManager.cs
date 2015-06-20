98354ae1bb5d1896fa0ba012874884106fdd1e83
Qian Wang <qwang2012@gmail.com>
2015-06-20 13:25:47 -0700

    Attempted to add networking to title/login

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -2,64 +2,78 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class TitleScreenManager : MonoBehaviour {
+public class TitleScreenManager : MonoBehaviour
+{
 
 	public GameObject errorText;
 	public Canvas canvas;
 	public Button playButton;
 	public Button settingsButton;
 	public InputField userNameField;
+	public GameObject playerList;
 
-	private Rect optionsWindow = new Rect((Screen.width / 2) - 100, Screen.height / 2 - 200, 200, 200);
+	private Rect optionsWindow = new Rect ((Screen.width / 2) - 100, Screen.height / 2 - 200, 200, 200);
 	private float volume = 1.0f;
 	private string clicked = "";
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-	public void onPlay(InputField userNameInput) {
+	public void setPlayerList (GameObject list)
+	{
+		playerList = list;
+	}
+
+	public void onPlay (InputField userNameInput)
+	{
 		if (!userNameInput.textComponent.text.Equals ("")) {
 			Debug.Log (userNameInput.textComponent.text);
-//			Application.LoadLevel("Champ Select Screen");
+			playerList.GetComponent<PlayerListManager> ().AddUsername (userNameInput.textComponent.text);
+			userNameInput.enabled = false;
+			//			Application.LoadLevel("Champ Select Screen");
 		} else {
-			GameObject obj = (GameObject) Instantiate(errorText, new Vector3(0, 0, 0), Quaternion.identity);
-			obj.transform.SetParent(canvas.transform, false);
+			GameObject obj = (GameObject)Instantiate (errorText, new Vector3 (0, 0, 0), Quaternion.identity);
+			obj.transform.SetParent (canvas.transform, false);
 		}
 	}
 
-	void OnGUI () {
-		if (clicked.Equals("Settings")) {
-			optionsWindow = GUI.Window(0, optionsWindow, doOptionsWindow, "My Window"); 
+	void OnGUI ()
+	{
+		if (clicked.Equals ("Settings")) {
+			optionsWindow = GUI.Window (0, optionsWindow, doOptionsWindow, "My Window"); 
 		}
-
 	}
 
 
-	public void onSettings(){
+	public void onSettings ()
+	{
 		clicked = "Settings";
 		toggleUIInteraction ();
 	}
 
-	void doOptionsWindow(int windowID) {
-		GUILayout.Box("Volume");
-		volume = GUILayout.HorizontalSlider(volume ,0.0f,1.0f);
+	void doOptionsWindow (int windowID)
+	{
+		GUILayout.Box ("Volume");
+		volume = GUILayout.HorizontalSlider (volume, 0.0f, 1.0f);
 		AudioListener.volume = volume;
-		if (GUILayout.Button("Back"))
-		{
+		if (GUILayout.Button ("Back")) {
 			clicked = "";
-			toggleUIInteraction();
+			toggleUIInteraction ();
 		}
-		GUI.DragWindow(new Rect (0,0,Screen.width,Screen.height));
+		GUI.DragWindow (new Rect (0, 0, Screen.width, Screen.height));
 	}
 
-	void toggleUIInteraction(){
+	void toggleUIInteraction ()
+	{
 		userNameField.interactable = !userNameField.interactable;
 		playButton.interactable = !playButton.interactable;
 		settingsButton.interactable = !settingsButton.interactable;
