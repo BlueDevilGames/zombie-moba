b6857820117b8d760c9e3f41534268c200923ba5
Sunjeev Devulapalli <svd12@duke.edu>
2015-06-20 15:06:10 -0400

    Added functional settings menu
    It will need some styling...

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -6,6 +6,13 @@ public class TitleScreenManager : MonoBehaviour {
 
 	public GameObject errorText;
 	public Canvas canvas;
+	public Button playButton;
+	public Button settingsButton;
+	public InputField userNameField;
+
+	private Rect optionsWindow = new Rect((Screen.width / 2) - 100, Screen.height / 2 - 200, 200, 200);
+	private float volume = 1.0f;
+	private string clicked = "";
 
 	// Use this for initialization
 	void Start () {
@@ -26,4 +33,36 @@ public class TitleScreenManager : MonoBehaviour {
 			obj.transform.SetParent(canvas.transform, false);
 		}
 	}
+
+	void OnGUI () {
+		if (clicked.Equals("Settings")) {
+			optionsWindow = GUI.Window(0, optionsWindow, doOptionsWindow, "My Window"); 
+		}
+
+	}
+
+
+	public void onSettings(){
+		clicked = "Settings";
+		toggleUIInteraction ();
+	}
+
+	void doOptionsWindow(int windowID) {
+		GUILayout.Box("Volume");
+		volume = GUILayout.HorizontalSlider(volume ,0.0f,1.0f);
+		AudioListener.volume = volume;
+		if (GUILayout.Button("Back"))
+		{
+			clicked = "";
+			toggleUIInteraction();
+		}
+		GUI.DragWindow(new Rect (0,0,Screen.width,Screen.height));
+	}
+
+	void toggleUIInteraction(){
+		userNameField.interactable = !userNameField.interactable;
+		playButton.interactable = !playButton.interactable;
+		settingsButton.interactable = !settingsButton.interactable;
+	}
+
 }
